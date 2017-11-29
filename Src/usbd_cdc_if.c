dd435104ed631b2bd773483afa1896c0e809c3a9
fanmuzhi <fanmuzhi@gmail.com>
2017-11-29 18:23:41 +0800

    increase vcp tx buff to 20480 bytes

--- Src/usbd_cdc_if.c
+++ Src/usbd_cdc_if.c
@@ -77,7 +77,7 @@
 /* Define size for the receive and transmit buffer over CDC */
 /* It's up to user to redefine and/or remove those define */
 #define APP_RX_DATA_SIZE  2048
-#define APP_TX_DATA_SIZE  2048
+#define APP_TX_DATA_SIZE  20480
 /* USER CODE END PRIVATE_DEFINES */
 /**
   * @}
